7cf2096f2b84b52ccc0c22c8bbe80e0c12f9ae25
roncan <65841016+roncan1@users.noreply.github.com>
2021-07-24 00:42:21 +0900

    Follow Papa Coding Course

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,20 @@ void main() {
   runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Well Made'),
+        ),
+        body: Center(
+            child: Image(
+                image: NetworkImage(
+                    "https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/22QT/image/p-RX98d_34y9ElK_Qfwz8OfHhxM.jpg"))),
+      ),
+    );
+  }
+}
+
