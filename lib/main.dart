71eaa5195b3bcc21de0631329c1578cdac5ab87f
roncan1 <carugame12_@naver.com>
2022-06-17 10:51:51 +0900

    0617 수행평가 코드1

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:learn_flutter/pages/recipe_page.dart';
+import 'package:learn_flutter/components/profile_buttons.dart';
+import 'package:learn_flutter/components/profile_count_info.dart';
+import 'package:learn_flutter/components/profile_drawer.dart';
+import 'package:learn_flutter/components/profile_header.dart';
+import 'package:learn_flutter/components/profile_tab.dart';
+import 'package:learn_flutter/theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: "PatuaOne"),
-      home: RecipePage(),
+      theme: theme(),
+      home: ProfilePage(),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      endDrawer: ProfileDrawer(),
+      appBar: _buildProfileAppBar(),
+      body: Column(
+        children: [
+          SizedBox(height: 20,),
+          ProfileHeader(),
+          SizedBox(height: 20,),
+          ProfileCountInfo(),
+          SizedBox(height: 20,),
+          ProfileButtons(),
+          Expanded(child: ProfileTab()),
+        ],
+      ),
+    );
+  }
+
+  AppBar _buildProfileAppBar() {
+    return AppBar(
+      leading: Icon(Icons.arrow_back_ios),
+      title: Text("Profile", style: TextStyle(color: Colors.black),),
+      centerTitle: true,
     );
   }
 }
